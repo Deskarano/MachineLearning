f485bdc3826b828b33362463d4f761c0db10d7c2
Gregor Haas <gregorhaas1997@gmail.com>
2016-11-05 20:53:09 -0400

    Made trig functions so much simpler

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import ml.charts.Graph;
+import ml.charts.elements.functions.TrigFunction;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        TrigFunction t = new TrigFunction(TrigFunction.SIN);
 
+        Graph g = new Graph(500, 500);
+        g.add(t, 0xFFFF0000);
+
+        g.display();
     }
 }
--- src/ml/charts/elements/functions/TrigFunction.java
+++ src/ml/charts/elements/functions/TrigFunction.java
@@ -9,63 +9,12 @@ public class TrigFunction extends Function
     public static final int CSC = 4;
     public static final int COT = 5;
 
-    private static Function cos;
-    private static Function sin;
-
     private int type;
-    private Function base;
 
     public TrigFunction(int type)
     {
         this.type = type;
-        if(cos == null || sin == null)
-        {
-            //generate the taylor polynomial of cos at x = pi (so we can compress all arguments to the range [0, 2pi)
-            double[] taylorCoefficients = new double[100];
-            double[] shift = new double[]{-Math.PI, 1};
 
-            //this is not computationally expensive, so we can calculate a lot of terms
-            for(int n = 0; n < taylorCoefficients.length; n++)
-            {
-                if(n % 2 == 0)
-                {
-                    taylorCoefficients[n] = Math.pow(-1, n / 2 + 1) / factorial(n);
-                }
-            }
-
-            cos = Function.concatenate(new PolynomialFunction(taylorCoefficients), new PolynomialFunction(shift));
-            sin = Function.concatenate(cos, new PolynomialFunction(new double[]{-Math.PI / 2, 1}));
-        }
-
-        switch(type)
-        {
-            case SIN:
-                base = sin;
-                break;
-
-            case COS:
-                base = cos;
-                break;
-
-            case TAN:
-                base = Function.divide(sin, cos);
-                break;
-
-            case SEC:
-                base = Function.divide(new PolynomialFunction(1), cos);
-                break;
-
-            case CSC:
-                base = Function.divide(new PolynomialFunction(1), sin);
-                break;
-
-            case COT:
-                base = Function.divide(cos, sin);
-                break;
-
-            default:
-                throw new IllegalArgumentException("what");
-        }
     }
 
     int getType()
@@ -76,28 +25,28 @@ public class TrigFunction extends Function
     @Override
     public double getValue(double x)
     {
-        while(x < 0)
+        switch(type)
         {
-            x += (2 * Math.PI);
-        }
+            case SIN:
+                return Math.sin(x);
 
-        while(x > 2 * Math.PI)
-        {
-            x -= (2 * Math.PI);
-        }
+            case COS:
+                return Math.cos(x);
 
-        return base.getValue(x);
-    }
+            case TAN:
+                return Math.tan(x);
 
-    private static double factorial(double n)
-    {
-        if(n == 0)
-        {
-            return 1;
-        }
-        else
-        {
-            return n * factorial(n - 1);
+            case SEC:
+                return 1 / Math.cos(x);
+
+            case CSC:
+                return 1 / Math.sin(x);
+
+            case COT:
+                return 1 / Math.tan(x);
+
+            default:
+                throw new IllegalArgumentException("what");
         }
     }
 }
